81a6ab82dfbfb770e104d13c0f52843f8a36b86e
HornyFerret <ombro.de.morto@gmail.com>
2021-07-11 16:54:16 +0300

    Добавьте файлы проекта.

---
+++ codewars-6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+namespace codewars_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "abracadabra";
+        }
+        public static int GetVowelCount(string str)
+        {
+            var strGL = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            var vowelCount = str.Count(s => strGL.Contains(s));
+            return vowelCount;
+        }
+    }
+}
